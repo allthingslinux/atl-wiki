3ceebcf09344834f9a9449de0a8f0157874d7e16
Atmois <info@atmois.com>
2025-08-18 20:01:10 +0100

    Remove old scripts

--- scripts/aclSetup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# /var/www
-sudo setfacl -R -m g:atl-sysadmins:rwx /var/www/atlwiki
-sudo setfacl -R -d -m g:atl-sysadmins:rwx /var/www/atlwiki
-
-# /var/www/atlwiki/images && /var/www/atlwiki/cache
-sudo setfacl -bR /var/www/atlwiki/images
-sudo setfacl -bR /var/www/atlwiki/cache
