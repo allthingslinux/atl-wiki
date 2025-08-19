dc95980385fff950d975abe64a4a99afc07c69cd
Atmois <info@atmois.com>
2025-08-19 13:35:27 +0100

    Move env stuff to the 00-Core file

--- LocalSettings.php
+++ LocalSettings.php
@@ -1,12 +1,5 @@
 <?php
 
-// Load environment variables from .env file using phpdotenv
-if (file_exists('/var/www/atlwiki/vendor/autoload.php')) {
-    require_once '/var/www/atlwiki/vendor/autoload.php';
-    $dotenv = Dotenv\Dotenv::createImmutable('/var/www/atlwiki');
-    $dotenv->safeLoad();
-}
-
 // Loads the config files in order
 $configFiles = glob('/var/www/atlwiki/configs/*.php');
 sort($configFiles);
